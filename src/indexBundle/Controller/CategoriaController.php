d5aa1e0476b16dba399d34baa3d3a9b5b7138377
davidcopano <davidcopano96@gmail.com>
2017-02-22 13:00:28 +0100

    Corrige error al enlazar 'baterías'

--- src/indexBundle/Controller/CategoriaController.php
+++ src/indexBundle/Controller/CategoriaController.php
@@ -6,9 +6,41 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CategoriaController extends Controller
 {
-    public function indexAction($categoria)
+    public function indexAction($nombre)
     {
+        // Ponemos la primera letra en mayúscula para buscar la categoría
 
+        $nombreDef = ucfirst(strtolower($nombre));
+
+        $em = $this->getDoctrine()->getManager();
+
+        $categoria = $this->getDoctrine()
+                            ->getRepository("indexBundle:Categoria")
+                            ->findOneBy(array("nombre" => $nombreDef));
+
+        if($categoria) {
+
+            $idCat = $categoria->getId();
+
+            $query = $em->createQuery(
+                "SELECT p.marca, p.modelo, p.precio, p.descripcion, i.url
+                FROM indexBundle:Producto p,
+                      indexBundle:Imagen i,
+                      indexBundle:ImagenTieneProducto itp,
+                      indexBundle:Categoria cat
+                WHERE p.id = itp.productoId
+                      AND itp.imagenId = i.id
+                      AND cat.id = p.categoriaId
+                      AND p.categoriaId = :idCat"
+            )->setParameter("idCat", $idCat);
+
+            $productos = $query->getResult();
+
+            return $this->render("indexBundle:Index:muestraCat.html.twig", array("productos" => $productos, "nombre" => $nombreDef));
+        }
+        else {
+            echo "no hay";exit;
+        }
     }
 
     public function categoriaAction()
